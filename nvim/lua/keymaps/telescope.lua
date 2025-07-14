bde852a112aa50cee30a219782db114495fa3b61
Daniel Rodriguez <daniel.rodriguezti1@gmail.com>
2025-07-14 22:49:56 +0200

    wofi menu customised

--- nvim/lua/keymaps/telescope.lua
+++
@@ -1,5 +0,0 @@
--- Eliminar todo el contenido y dejar solo:
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Buscar archivos" })
-vim.keymap.set("n", "<C-g>", "<cmd>Telescope live_grep<cr>", { desc = "Buscar texto (live grep)" })
-vim.keymap.set("n", "<C-b>", "<cmd>Telescope buffers<cr>", { desc = "Buscar buffers" })
-vim.keymap.set("n", "<C-h>", "<cmd>Telescope help_tags<cr>", { desc = "Buscar ayuda" })
